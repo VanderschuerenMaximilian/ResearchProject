f6dd14afe8a05f52c7f63a171cf7dde70d4a1403
VanderschuerenMaximilian <91435601+VanderschuerenMaximilian@users.noreply.github.com>
2024-01-07 15:50:29 +0100

    feat: sveltekit setup

---
+++ packages/frontend/src/routes/testroute/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<title>Test Route</title>
+	<meta name="description" content="My testing route" />
+</svelte:head>
+
+<div>
+    <h1>
+        Test Route
+    </h1>
+
+    <p>
+        This is a test route.
+    </p>
+</div>
