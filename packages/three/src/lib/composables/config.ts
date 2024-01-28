7895b85c5b36efbf25b45dd391fa493d05bd1bb6
VanderschuerenMaximilian <91435601+VanderschuerenMaximilian@users.noreply.github.com>
2024-01-28 18:19:23 +0100

    feat: prepare for zip

--- packages/three/src/lib/composables/config.ts
+++ packages/three/src/lib/composables/config.ts
@@ -1,3 +1,3 @@
 import { writable } from 'svelte/store'
 //Add your server url here in the " " below
-export const cogniflowServer = writable<string>("https://b64f-2a02-a03f-e1a1-b901-ed50-9686-1df6-102c.ngrok-free.app")
+export const cogniflowServer = writable<string>("https://7f99-2a02-a03f-e1a1-b901-6c15-afc3-9fcd-299a.ngrok-free.app")
